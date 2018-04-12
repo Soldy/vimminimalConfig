0e811f136bbf8d8275c99b0ae3b038ee4ee15b50
Soldy <testa.a.tapos@gmail.com>
2018-04-12 19:20:56 +0100

    better way

--- viminstall.sh
+++ viminstall.sh
@@ -46,9 +46,10 @@ echo "set runtimepath+=~/.vim/bundle/vim-fugitive/plugin/fugitive.vim" >> ~/.vim
 
 git clone https://github.com/scrooloose/nerdtree.git 
 vim -u NONE -c "helptags nerdtree/doc" -c q
+echo "set runtimepath+=~/.vim/bundle/nerdtree" >> ~/.vimrc
 echo "set runtimepath+=~/.vim/bundle/nerdtree/plugin/NERD_tree.vim" >> ~/.vimrc
-echo "autocmd StdinReadPre * let s:std_in=1" >> ~/.vimrc
-echo "autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif" >> ~/.vimrc
+#echo "autocmd StdinReadPre * let s:std_in=1" >> ~/.vimrc
+#echo "autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif" >> ~/.vimrc
 echo "map <C-\> :NERDTreeToggle<CR>" >> ~/.vimrc
 echo "let g:neocomplete#enable_at_startup = 1" >> ~/.vimrc
 echo "let NERDTreeMapOpenInTab='\r'" >> ~/.vimrc
@@ -58,26 +59,28 @@ echo "let NERDTreeMapOpenInTab='\r'" >> ~/.vimrc
 
 git clone https://github.com/vim-airline/vim-airline.git
 vim -u NONE -c "helptags vim-airline/doc" -c q
+echo "set runtimepath+=~/.vim/bundle/vim-airline" >> ~/.vimrc
 echo "set runtimepath+=~/.vim/bundle/vim-airline/plugin/airline.vim" >> ~/.vimrc
 echo "let g:airline#extensions#tabline#enabled = 1" >> ~/.vimrc
 echo "let g:airline#extensions#tabline#formatter = 'default'" >> ~/.vimrc
 echo "let g:airline_detect_iminsert=0" >> ~/.vimrc
 echo "let g:airline_left_sep = '▶'" >> ~/.vimrc
 echo "let g:airline_right_sep = ''" >> ~/.vimrc
-echo "let g:airline_theme='murmur'" >> ~/.vimrc
 
 
 #Airline-themes
 
 git clone https://github.com/vim-airline/vim-airline-themes.git
 vim -u NONE -c "helptags vim-airline-themes/doc" -c q
+echo "set runtimepath+=~/.vim/bundle/vim-airline-themes" >> ~/.vimrc
 echo "set runtimepath+=~/.vim/bundle/vim-airline-themes/plugin/airline-themes.vim" >> ~/.vimrc
-
+echo "let g:airline_theme='murmur'" >> ~/.vimrc
 
 #syntastic
 
 git clone https://github.com/vim-syntastic/syntastic.git
 vim -u NONE -c "helptags syntastic/doc" -c q
+echo "set runtimepath+=~/.vim/bundle/syntastic" >> ~/.vimrc
 echo "set runtimepath+=~/.vim/bundle/syntastic/plugin/syntastic.vim" >> ~/.vimrc
 
 
@@ -85,6 +88,7 @@ echo "set runtimepath+=~/.vim/bundle/syntastic/plugin/syntastic.vim" >> ~/.vimrc
 
 git clone https://github.com/Shougo/neocomplete.vim.git
 vim -u NONE -c "helptags neocomplete.vim/doc" -c q
+echo "set runtimepath+=~/.vim/bundle/neocomplete.vim" >> ~/.vimrc
 echo "set runtimepath+=~/.vim/bundle/neocomplete.vim/plugin/neocomplete.vim" >> ~/.vimrc
 echo "au FileType php setl ofu=phpcomplete#CompletePHP" >> ~/.vimrc
 echo "au FileType ruby,eruby setl ofu=rubycomplete#Complete" >> ~/.vimrc
@@ -98,8 +102,8 @@ echo "au FileType css setl ofu=csscomplete#CompleteCSS" >> ~/.vimrc
 ###################
 
 echo "install vim themes"
-mkdir ~/.vim/color
-cd ~/.vim/color
+mkdir ~/.vim/colors
+cd ~/.vim/colors
 
 
 # jellybeans
